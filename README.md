8e52266c3680d4df7f306fce23e54544b3ca0241
axpecial <and043@ucsd.edu>
2018-10-20 12:59:43 -0700

    First commit + Completed required user stories

---
+++ README.md
@@ -0,0 +1,22 @@
+# MemFlash
+
+## App Description
+Prepare for an upcoming exam faster with MemFlash.
+Challenge yourself with a question, try your best to answer, then tap the screen to flip the card and check if you got it right.
+
+## App Walk-though
+<img src="https://i.imgur.com/EpbfrRH.gif" width=200><br>
+
+## Lab 1 User Stories
+### Required
+- [x] Create New Project in Android Studio
+- [x] Add a view for the front side of the flashcard to display the question
+- [x] Add a view for the back side of the flashcard to display the answer
+- [x] Build in logic to show the answer side when the card is tapped
+- [x] Push code to GitHub
+### Optional
+- [x] Toggle the flashcard between the question side and the answer side
+- [ ] Style the question and answer side of the card to better distinguish between the two sides
+- [ ] Add selectable multiple choice answers beneath the card
+- [ ] Change the background color of the multiple choice answers when clicked to indicate whether the question
+- [ ] Further customize and style the card
---
+++ app/src/main/java/com/example/andyduong/memflash/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.andyduong.memflash;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Links the XML layout file to this Java file, which handles the user interaction for the
+        // corresponding activity
+        setContentView(R.layout.activity_main);
+
+        // Sets onClick for flashcard question
+        findViewById(R.id.flashcard_question).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.flashcard_question).setVisibility(View.INVISIBLE);
+                findViewById(R.id.flashcard_answer).setVisibility(View.VISIBLE);
+            }
+        });
+
+        // Sets onClick for flashcard answer
+        findViewById(R.id.flashcard_answer).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.flashcard_answer).setVisibility(View.INVISIBLE);
+                findViewById(R.id.flashcard_question).setVisibility(View.VISIBLE);
+            }
+        });
+
+
+
+    }
+}
