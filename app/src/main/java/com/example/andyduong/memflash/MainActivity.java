317f6529ea64ec942f8fa816b6ad37a21dd300e5
axpecial <and043@ucsd.edu>
2018-10-21 10:02:18 -0700

    Completed the optionals and Adjusted the UI for Nexus 5

--- app/src/main/java/com/example/andyduong/memflash/MainActivity.java
+++ app/src/main/java/com/example/andyduong/memflash/MainActivity.java
@@ -3,8 +3,12 @@ package com.example.andyduong.memflash;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int correct_answer_id = R.id.flashcard_answer_4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +35,97 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // Sets onClick for show hint button
+        findViewById(R.id.show_hint_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                findViewById(R.id.show_hint_button).setVisibility(View.INVISIBLE);
+                findViewById(R.id.hide_hint_button).setVisibility(View.VISIBLE);
+                showAllAnswers();
+            }
+        });
 
+        // Sets onClick for hide hint button
+        findViewById(R.id.hide_hint_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                findViewById(R.id.hide_hint_button).setVisibility(View.INVISIBLE);
+                findViewById(R.id.show_hint_button).setVisibility(View.VISIBLE);
+                hideAllAnswers();
+            }
+        });
 
+        // Sets onClick for flashcard answer 1 button
+        findViewById(R.id.flashcard_answer_1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (correct_answer_id != R.id.flashcard_answer_1) {
+                    markIncorrectAnswer(R.id.flashcard_answer_1);
+                }
+                showCorrectAnswer();
+            }
+        });
+
+        // Sets onClick for flashcard answer 2 button
+        findViewById(R.id.flashcard_answer_2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (correct_answer_id != R.id.flashcard_answer_2) {
+                    markIncorrectAnswer(R.id.flashcard_answer_2);
+                }
+                showCorrectAnswer();
+            }
+        });
+
+        // Sets onClick for flashcard answer 3 button
+        findViewById(R.id.flashcard_answer_3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (correct_answer_id != R.id.flashcard_answer_3) {
+                    markIncorrectAnswer(R.id.flashcard_answer_3);
+                }
+                showCorrectAnswer();
+            }
+        });
+
+        // Sets onClick for flashcard answer 1 button
+        findViewById(R.id.flashcard_answer_4).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (correct_answer_id != R.id.flashcard_answer_4) {
+                    markIncorrectAnswer(R.id.flashcard_answer_4);
+                }
+                showCorrectAnswer();
+            }
+        });
+
+    }
+
+    private void showAllAnswers() {
+        findViewById(R.id.flashcard_answer_1).setVisibility(View.VISIBLE);
+        findViewById(R.id.flashcard_answer_2).setVisibility(View.VISIBLE);
+        findViewById(R.id.flashcard_answer_3).setVisibility(View.VISIBLE);
+        findViewById(R.id.flashcard_answer_4).setVisibility(View.VISIBLE);
+    }
+
+    private void hideAllAnswers() {
+        findViewById(R.id.flashcard_answer_1).setVisibility(View.INVISIBLE);
+        findViewById(R.id.flashcard_answer_2).setVisibility(View.INVISIBLE);
+        findViewById(R.id.flashcard_answer_3).setVisibility(View.INVISIBLE);
+        findViewById(R.id.flashcard_answer_4).setVisibility(View.INVISIBLE);
+    }
+
+    private void showCorrectAnswer() {
+        findViewById(correct_answer_id).setBackgroundResource(R.drawable.card_background_correct);
+        ((TextView) findViewById(correct_answer_id)).setTextColor(
+                getResources().getColor(R.color.bsWhite, null)
+        );
+    }
+
+    private void markIncorrectAnswer(int id) {
+        findViewById(id).setBackgroundResource(R.drawable.card_background_incorrect);
+        ((TextView) findViewById(id)).setTextColor(
+                getResources().getColor(R.color.bsWhite, null)
+        );
     }
 }
